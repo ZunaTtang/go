f846ef048136a9ce0738f14253540fd13b89c709
PILGYU JANG <maxjang123@naver.com>
2021-10-23 22:53:23 +0900

    연동

--- lesson.go
+++ lesson.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!!")
 	fmt.Println("view->terminal->go run lesson.go")
+	fmt.Println("깃허브에서 리포지토리->new->이름적고 git remote add origin 부분 폴더cmd에서 복붙해서 연동")
 	fmt.Println("git init")
 	fmt.Println("git add .")
 	fmt.Println("git status")
