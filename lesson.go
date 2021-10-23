13ab948c0c4231d1694337f5fc1f4dbce3aa75e7
PILGYU JANG <maxjang123@naver.com>
2021-10-23 22:46:38 +0900

    second commit

--- lesson.go
+++ lesson.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!!")
+	fmt.Println("view->terminal->go run lesson.go")
 	fmt.Println("git init")
-	fmt Println("git status")
 	fmt.Println("git add .")
+	fmt.Println("git status")
+	fmt.Println("git commit -m ''initial commit'' ")
+	fmt.Println("git push origin main")
+	fmt.Println("git pull origin main ")
+	fmt.Println("git reset --hard 해시나 링크복붙")
 
 }
